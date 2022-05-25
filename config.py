1273612e72f880c49214cdf7ed9cb448c93b10bf
Aneeta Ann John <73518251+aneetaann@users.noreply.github.com>
2022-05-25 11:56:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,5 +15,5 @@ connectionstring = ''    #DatabaseServer:DatabasePort/SID
 #For the following error: "DPI-1047: Cannot locate a 64-bit Oracle Client library: The specified module could not be found"
 #Download 64-bit version of oracle instantClient (Basic Package) from: https://www.oracle.com/database/technologies/instant-client/winx64-64-downloads.html
 #Extract the files in the instantclient_directory.zip file to the python directory
-#Copy the path of the extracted folder and paste it in line 6 of the dsr.py code
+#Copy the path of the extracted folder and paste it in line 6 of the healthcheckreport.py code
 
